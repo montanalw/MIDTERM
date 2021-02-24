8805cd6477d9002ea4433a3f3ba79215d7f7bbd1
younsoolim <ylim27@my.bcit.ca>
2021-02-24 04:37:52 -0800

    changed html, js, css according to initial comps files

--- main.js
+++ main.js
@@ -5,68 +5,67 @@ window.onload = function() {
   $("#wrongAns2").hide()
   $("#correctAns2").hide()
 
+  // function question1() {
+  //   let li = $("#water_options").children()
 
-  function question1() {
-    let li = $("#water_options").children()
+  //   for(i=0; i < li.length; i++){
+  //     if(i<3){
+  //       li[i].onclick = function() {
+  //         $("#correctAns1").hide()
+  //         $("#wrongAns1").show()
+  //         li[3].style.backgroundColor = "#fff"             
+  //       }
+  //     } 
+  //     else{
+  //       li[i].onclick = function() {
+  //         $("#correctAns1").show()
+  //         $("#wrongAns1").hide()
+  //         li[3].style.backgroundColor = "#D7F040"   
+  //       }
+  //     }
+  //   }
+  // }
 
-    for(i=0; i < li.length; i++){
-      if(i<3){
-        li[i].onclick = function() {
-          $("#correctAns1").hide()
-          $("#wrongAns1").show()
-          li[3].style.backgroundColor = "#fff"             
-        }
-      } 
-      else{
-        li[i].onclick = function() {
-          $("#correctAns1").show()
-          $("#wrongAns1").hide()
-          li[3].style.backgroundColor = "#D7F040"   
-        }
-      }
-    }
-  }
+  // function question2() {
+  //   let li = $("#decom_options").children()
 
-  function question2() {
-    let li = $("#decom_options").children()
+  //   for(i=0; i < li.length; i++){
 
-    for(i=0; i < li.length; i++){
+  //     if(i<2 || i>2){
+  //       li[i].onclick = function() {
+  //         $("#correctAns2").hide()
+  //         $("#wrongAns2").show()
+  //         li[2].style.backgroundColor = "#fff"             
+  //       }
+  //     }       
+  //     else{
+  //       li[i].onclick = function() {
+  //         $("#correctAns2").show()
+  //         $("#wrongAns2").hide()
+  //         li[2].style.backgroundColor = "#D7F040"   
+  //       }
+  //     } 
+  //   }
+  // }
 
-      if(i<2 || i>2){
-        li[i].onclick = function() {
-          $("#correctAns2").hide()
-          $("#wrongAns2").show()
-          li[2].style.backgroundColor = "#fff"             
-        }
-      }       
-      else{
-        li[i].onclick = function() {
-          $("#correctAns2").show()
-          $("#wrongAns2").hide()
-          li[2].style.backgroundColor = "#D7F040"   
-        }
-      } 
-    }
-  }
+  // function wrongAns1() {
+  //   let girl = document.querySelector("#sad1")
+  //   girl.onclick = function() {      
+  //     $("#wrongAns1").hide()  
+  //   }
+  // }
 
-  function wrongAns1() {
-    let girl = document.querySelector("#sad1")
-    girl.onclick = function() {      
-      $("#wrongAns1").hide()  
-    }
-  }
+  // function wrongAns2() {    
+  //   let girl = document.querySelector("#sad2")
+  //   girl.onclick = function() { 
+  //     $("#wrongAns2").hide()
+  //   }
+  // }
 
-  function wrongAns2() {    
-    let girl = document.querySelector("#sad2")
-    girl.onclick = function() { 
-      $("#wrongAns2").hide()
-    }
-  }
-
-  question1()
-  question2()
-  wrongAns1() 
-  wrongAns2() 
+  // question1()
+  // question2()
+  // wrongAns1() 
+  // wrongAns2() 
 
 }
 
