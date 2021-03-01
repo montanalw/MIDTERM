a8b8eb95d23ed0b4cff8e503d5b8b93340ebb978
irenewuu <irenewu222@gmail.com>
2021-03-01 10:35:57 -0800

    fixed section b's js file

--- js/sadgirl.js
+++ js/sadgirl.js
@@ -1,5 +1,5 @@
-function thinkingClicked()
-{
+// function thinkingClicked()
+// {
     let thinking = document.getElementById("thinking")
     let appear = document.getElementById("hide")
     
@@ -8,5 +8,5 @@ function thinkingClicked()
             thinking.style.display = "none";
             appear.style.visibility = "visible";
     }
-}
-window.onload = thinkingClicked
+// }
+// window.onload = thinkingClicked
