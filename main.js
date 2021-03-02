6d81c89f0b6357fe02ddbae821aafa3d9c61502a
younsoolim <ylim27@my.bcit.ca>
2021-03-02 11:32:15 -0800

    initial commit

--- main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-
-  document.querySelector("#wrongAns1").hide()
-  document.querySelector("#correctAns1").hide()
-  document.querySelector("#wrongAns2").hide()
-  document.querySelector("#correctAns2").hide()
-
-}
-
