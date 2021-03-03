9de2f124fa5f0cf6aa28e2edc00819a89833811a
montanalw <72103298+montanalw@users.noreply.github.com>
2021-03-02 20:30:46 -0800

    trying to get the gradient working lol

--- js/thread.js
+++ js/thread.js
@@ -2,7 +2,21 @@ function makeVisible() {
     var x = document.getElementById("big-div");
     if (x.style.display === "none") {
       x.style.display = "block";
-    } else {
+    }
+    else {
       x.style.display = "none";
     }
+
   }
+
+  function bgCol() {
+    var bgCol = document.getElementById("frontpage");
+
+    if (bgCol.style.display === "block")
+    {
+      bgCol.style.backgroundImage = "linear-gradient(#CCE6F4, #CCE6F4, #fcdb58);"
+    }
+    
+  }
+
+  window.onload = bgCol
